85354a09618e3f705443b1b5b94f5666627c4917
jLaszczewski <j.laszczewski@icloud.com>
2018-04-11 15:51:35 +0200

    Simple codable model (we don't need list of stories) and BREAK FOR NOW

---
+++ LiveStories/Model/LiveStory.swift
@@ -0,0 +1,23 @@
+//
+//  LiveStory.swift
+//  LiveStories
+//
+//  Created by Jakub Łaszczewski on 11.04.2018.
+//  Copyright © 2018 Jakub Łaszczewski. All rights reserved.
+//
+
+import Foundation
+
+class Employee: Codable {
+    var month: String
+    var num: Int
+    var link: String
+    var year: String
+    var news: String
+    var safe_title: String
+    var transcript: String
+    var alt: String
+    var img: String
+    var title: String
+    var day: String
+}
