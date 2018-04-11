8a317275d37faddb3b0b62f5f12e7be5d359e2a3
jLaszczewski <j.laszczewski@icloud.com>
2018-04-11 15:41:07 +0200

    swipe left / swipe right gestures

--- LiveStories/ViewController.swift
+++ LiveStories/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class LiveStoriesMainController: UIViewController {
-
+    var swipeBack = SwipeBack()
+    var swipeGestureRecognizer: UIPanGestureRecognizer!
+    
+    var swipeFront = SwipeFront()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        swipeGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panned))
+        self.view.addGestureRecognizer(swipeGestureRecognizer)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func panned(gestureRecognizer: UIPanGestureRecognizer) {
+        if gestureRecognizer.location(in: self.view).x <= 0.3 * UIScreen.main.bounds.size.width {
+            let gestureState = swipeBack.backSwipe(gestureRecognizer: gestureRecognizer, sourceViewController: self)
+            switch gestureState {
+            case .cancelled, .ended, .failed:
+                swipeBack = SwipeBack()
+            default:
+                break
+            }
+        } else if gestureRecognizer.location(in: self.view).x >= 0.3 * UIScreen.main.bounds.size.width {
+            let gestureState = swipeFront.frontSwipe(gestureRecognizer: gestureRecognizer, sourceViewController: self)
+            switch gestureState {
+            case .cancelled, .ended, .failed:
+                swipeFront = SwipeFront()
+            default:
+                break
+            }
+        }
     }
-
 
 }
 
---
+++ LiveStories/public/SwipeFront.swift
@@ -0,0 +1,88 @@
+//
+//  SwipeFront.swift
+//  LiveStories
+//
+//  Created by Jakub Łaszczewski on 11.04.2018.
+//  Copyright © 2018 Jakub Łaszczewski. All rights reserved.
+//
+
+import UIKit
+
+class SwipeFront: NSObject {
+    var screenWidth: CGFloat!
+    var screenHeight: CGFloat!
+    var sourceView: UIView!
+    var destinationView: UIView!
+    var sourceViewController: UIViewController!
+    var destinationViewController: UIViewController!
+    var window: UIWindow?
+    let destinationViewXPosition = UIScreen.main.bounds.width / 2
+    var isSwipeBackBegan = false
+    
+    func frontSwipe(gestureRecognizer: UIPanGestureRecognizer, sourceViewController: UIViewController) -> UIGestureRecognizerState {
+        switch gestureRecognizer.state {
+        case .began:
+            self.sourceViewController = sourceViewController
+            destinationViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "mainStoryboard") as! LiveStoriesMainController
+            
+            sourceView = sourceViewController.view!
+            destinationView = destinationViewController.view!
+            
+            screenWidth = sourceView.frame.width
+            screenHeight = sourceView.frame.height
+            
+            sourceView.addShadow()
+            sourceView.layer.shadowOffset = CGSize(width: 7, height: 3)
+            sourceView.layer.shadowOpacity = 0.5
+            
+            destinationView.frame = CGRect(x: destinationViewXPosition, y: sourceView.frame.minY, width: sourceView.frame.width, height: sourceView.frame.height)
+            
+            window = UIApplication.shared.keyWindow
+            window?.insertSubview(destinationView, belowSubview: sourceView)
+            isSwipeBackBegan = true
+        case .changed:
+            if isSwipeBackBegan {
+                let xTranslation = gestureRecognizer.translation(in: sourceView).x
+                let destinationViewXOffset = (xTranslation / screenWidth) * (destinationViewXPosition)
+                UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseOut, animations: { () -> Void in
+                    if self.sourceView.frame.offsetBy(dx: xTranslation, dy: 0.0).minX < 0 {
+                        self.sourceView.frame = self.sourceView.frame.offsetBy(dx: xTranslation, dy: 0.0)
+                        self.destinationView.frame = self.destinationView.frame.offsetBy(dx: destinationViewXOffset, dy: 0.0)
+                        self.sourceView.layer.shadowOffset = CGSize(width: self.sourceView.layer.shadowOffset.width
+                            - (xTranslation / self.screenWidth) * 7, height: self.sourceView.layer.shadowOffset.height)
+                        self.sourceView.layer.shadowOpacity = self.sourceView.layer.shadowOpacity + Float(xTranslation / self.screenWidth) * 0.5
+                    }
+                })
+                
+                gestureRecognizer.setTranslation(CGPoint.zero, in: sourceView)
+            }
+        case .ended, .cancelled:
+            if isSwipeBackBegan {
+                if sourceView.frame.origin.x <= -0.4 * screenWidth {
+                    
+                    gestureRecognizer.isEnabled = true
+                    UIView.animate(withDuration: 0.25, delay: 0, options: .curveEaseOut, animations: { () -> Void in
+                        self.sourceView.frame.origin.x = -self.screenWidth
+                        self.destinationView.frame.origin.x = 0
+                        self.sourceView.backShadowAnimation(opacityFrom: self.sourceView.layer.shadowOpacity, offsetFrom:  self.sourceView.layer.shadowOffset, duration: 0.25)
+                        
+                    }) { (finish) -> Void in
+                        self.window = nil
+                        customSegue(destinationViewController: self.destinationViewController)
+                    }
+                } else {
+                    UIView.animate(withDuration: 0.25, delay: 0, options: .curveEaseOut, animations: { () -> Void in
+                        self.sourceView.frame.origin.x = 0
+                        self.destinationView.frame.origin.x = self.destinationViewXPosition
+                    }) { (finish) -> Void in
+                        self.window = nil
+                        self.destinationView.removeFromSuperview()
+                    }
+                }
+            }
+        default:
+            break
+        }
+        return gestureRecognizer.state
+    }
+}
